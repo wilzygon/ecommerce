60ee1ec810afd9268e17066bc7a8f9a32f0dcee8
Wilson Osvaldo <wilzygon@gmail.com>
2023-10-22 17:44:01 -0300

    Agregando main

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/wilzygon/ecommerce/infrastructure/handler/response"
+)
+
+func main() {
+	err := loadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = validateEnvironments()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Crear nuestro echo
+	//echo la librería que utilizamos para routing
+	e := newHTTP(response.HTTPErrorHandler)
+
+	//Instanciamos la conexión a la base de datos
+	dbpool, err := newDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = dbpool
+
+	//Subimos nuestro Servicio al puerto configurado
+	err = e.Start(":" + os.Getenv("SERVER_PORT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
