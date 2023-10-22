decd34f22decb7383b977a6ff708afeb80377e1d
Wilson Osvaldo <wilzygon@gmail.com>
2023-10-22 20:59:12 -0300

    Estandarizando respuestas

--- infrastructure/handler/response/message.go
+++ infrastructure/handler/response/message.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"github.com/wilzygon/ecommerce/model"
 )
--- infrastructure/handler/user/handler.go
+++ infrastructure/handler/user/handler.go
@@ -1,15 +1,15 @@
 package user
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/wilzygon/ecommerce/domain/user"
+	"github.com/wilzygon/ecommerce/infrastructure/handler/response"
 	"github.com/wilzygon/ecommerce/model"
 )
 
 type handler struct {
-	useCase user.UseCase //UseCase es la interface que se conecta con el domino, y handler lo conoce
+	useCase   user.UseCase //UseCase es la interface que se conecta con el domino, y handler lo conoce
+	responser response.API
 }
 
 func newHandler(uc user.UseCase) handler { //Cuando creamos un nuevo handler le enviamos ese UseCase, ya
@@ -20,22 +20,25 @@ func (h handler) Create(c echo.Context) error {
 	m := model.User{}
 
 	if err := c.Bind(&m); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+		return h.responser.BindFailed(err)
+		//return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
 
 	if err := h.useCase.Create(&m); err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		return h.responser.Error(c, "useCase.Create()", err)
+		//return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, m)
+	return c.JSON(h.responser.Created(m))
+	//return c.JSON(http.StatusCreated, m)
 }
 
 func (h handler) GetAll(c echo.Context) error {
 	users, err := h.useCase.GetAll()
 	if err != nil {
-
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		return h.responser.Error(c, "useCase.GetAll()", err)
+		//return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 	}
-
-	return c.JSON(http.StatusOK, users)
+	return c.JSON(h.responser.Ok(users))
+	//return c.JSON(http.StatusOK, users)
 }
